696434c4e99391d3fb969197b7e8effb240f4b0b
Edcreation <93517096+Edcreation@users.noreply.github.com>
2023-01-12 23:33:37 +0200

    great change

--- navbar.js
+++ navbar.js
@@ -3,11 +3,13 @@ function toggleNav(){
   if(open==false){
       navUl = document.getElementById("actions");
       navUl.style.display="block";
+      //navUl.style.animation = 	"animation: slideInFromLeft .8s;"
       open = true;
-  }else{
-        navUl = document.getElementById("actions");
-        navUl.style.display="none";
-        open = false; 
+    }else{
+      navUl = document.getElementById("actions");
+      //navUl.style.animation = 	"slide-out-left 0.5s cubic-bezier(0.550, 0.085, 0.680, 0.530) both"
+      navUl.style.display="none";
+      open = false; 
   }
 }
 function toggleDashNav() {
