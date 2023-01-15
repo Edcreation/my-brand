1b6834442ad43751527e63e54ac11cc0fb1ce3b8
Edcreation <93517096+Edcreation@users.noreply.github.com>
2023-01-15 11:15:50 +0200

    Nav menu changed

--- navbar.js
+++ navbar.js
@@ -1,7 +1,15 @@
 let open = false;    
 function toggleNav(){
+  var myImage = document.getElementById('ig');
+  myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === './images/open.png') {
+        myImage.setAttribute ('src','./images/close.png');
+    } else {
+        myImage.setAttribute ('src','./images/open.png');
+    }
   if(open==false){
-      navUl = document.getElementById("actions");
+    navUl = document.getElementById("actions");
       navUl.style.display="block";
       //navUl.style.animation = 	"animation: slideInFromLeft .8s;"
       open = true;
@@ -10,8 +18,9 @@ function toggleNav(){
       //navUl.style.animation = 	"slide-out-left 0.5s cubic-bezier(0.550, 0.085, 0.680, 0.530) both"
       navUl.style.display="none";
       open = false; 
+    }
   }
-}
+};
 function toggleDashNav() {
   if(open==false){
     navUl = document.getElementById("dash-nav");
