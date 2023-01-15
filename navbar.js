27f03c7dcbc172607d658998dd484de6e424e02d
Edcreation <93517096+Edcreation@users.noreply.github.com>
2023-01-15 11:35:19 +0200

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,25 +1,21 @@
 let open = false;    
 function toggleNav(){
   var myImage = document.getElementById('ig');
-  myImage.onclick = function() {
-    var mySrc = myImage.getAttribute('src');
-    if(mySrc === './images/open.png') {
-        myImage.setAttribute ('src','./images/close.png');
-    } else {
-        myImage.setAttribute ('src','./images/open.png');
-    }
   if(open==false){
+    var mySrc = myImage.getAttribute('src');
     navUl = document.getElementById("actions");
+    myImage.setAttribute ('src','./images/close.png');
       navUl.style.display="block";
       //navUl.style.animation = 	"animation: slideInFromLeft .8s;"
       open = true;
     }else{
+      myImage.setAttribute ('src','./images/open.png');
       navUl = document.getElementById("actions");
       //navUl.style.animation = 	"slide-out-left 0.5s cubic-bezier(0.550, 0.085, 0.680, 0.530) both"
       navUl.style.display="none";
       open = false; 
     }
-  }
+  
 };
 function toggleDashNav() {
   if(open==false){
